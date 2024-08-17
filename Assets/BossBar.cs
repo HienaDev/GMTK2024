aeff7f9bf8f1d285dfbf4d47f72bea512d29a053
Julia Costa <juliacosta232005@gmail.com>
2024-08-17 20:46:48 +0100

    Add Shark HealthManager

--- Assets/BossBar.cs
+++ Assets/BossBar.cs
@@ -8,18 +8,20 @@ public class BossBar : MonoBehaviour
 {
     [SerializeField] private Image healthBar;
     [SerializeField] private TextMeshProUGUI healthText;
-    [SerializeField] private float bossHealth;
-    [SerializeField] private float maxHealth;
+    private float maxHealth;
+    private float bossHealth;
+    private GameObject bossObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        bossHealth = maxHealth;
+        maxHealth = BossHealthManager.BossHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        bossHealth = BossHealthManager.BossHealth;
         healthBar.fillAmount = Mathf.InverseLerp(0f,maxHealth,bossHealth);
         healthText.text = $"{bossHealth:f0}/{maxHealth}";
     }
