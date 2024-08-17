9eb4c091a1b9646caba0136af9708a691c917423
HienaDev <ant.rodrigues1999@gmail.com>
2024-08-17 20:41:45 +0100

    Add intro to main menu

--- Assets/Scripts/HurricanPhase.cs
+++ Assets/Scripts/HurricanPhase.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MinePhase : MonoBehaviour
+public class HurricanPhase : MonoBehaviour
 {
     [SerializeField] private float phaseDuration = 30;
 
-    
 
-    [SerializeField] private GameObject minePrefab;
+
+    [SerializeField] private GameObject hurricanePrefab;
     [SerializeField] private float spawnRate;
     private float justSpawned;
     [SerializeField] private Vector2 area = new Vector2(200, 110);
 
 
     private float startedPhase;
+
+    private bool phaseEnded = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,14 +31,15 @@ public class MinePhase : MonoBehaviour
         {
 
             justSpawned = Time.time;
-            GameObject mineClone = Instantiate(minePrefab);
-            mineClone.transform.position = new Vector2(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y));
+            GameObject hurricaneClone = Instantiate(hurricanePrefab);
+            hurricaneClone.transform.position = new Vector2(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y));
 
 
         }
-        else if (Time.time - startedPhase >= phaseDuration)
+        else if (Time.time - startedPhase >= phaseDuration && !phaseEnded)
         {
-            
+            PhaseManager.Instance.PhaseEnded();
+            phaseEnded = true;
         }
     }
 }
--- Assets/Scripts/MinePhase.cs
+++ Assets/Scripts/MinePhase.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HurricanPhase : MonoBehaviour
+public class MinePhase : MonoBehaviour
 {
     [SerializeField] private float phaseDuration = 30;
 
+    
 
-
-    [SerializeField] private GameObject hurricanePrefab;
+    [SerializeField] private GameObject minePrefab;
     [SerializeField] private float spawnRate;
     private float justSpawned;
     [SerializeField] private Vector2 area = new Vector2(200, 110);
 
 
     private float startedPhase;
+
+    private bool phaseEnded = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,14 +31,15 @@ public class HurricanPhase : MonoBehaviour
         {
 
             justSpawned = Time.time;
-            GameObject hurricaneClone = Instantiate(hurricanePrefab);
-            hurricaneClone.transform.position = new Vector2(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y));
+            GameObject mineClone = Instantiate(minePrefab);
+            mineClone.transform.position = new Vector2(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y));
 
 
         }
         else if (Time.time - startedPhase >= phaseDuration)
         {
             PhaseManager.Instance.PhaseEnded();
+            phaseEnded = true;
         }
     }
 }
