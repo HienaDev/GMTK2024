68152494a31aae6e3da1af08ad263e0230842f1d
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-08-18 16:40:36 +0100

    Directional movement animation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,26 +5,18 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed;
+    private float           moveSpeed;
     [SerializeField]
-    private Rigidbody2D rb2d;
-    private Vector2 moveInput;
-    private SpriteRenderer sprite;
-    
-    //playarea bounds
-    private float minX, maxX, minY, maxY;
+    private Rigidbody2D     rb2d;
+    private Vector2         moveInput;
+    private SpriteRenderer  sprite;
+    private Animator        animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        //get camera bounds
-        Camera cam = Camera.main;
-        minX = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane)).x;
-        maxX = cam.ViewportToWorldPoint(new Vector3(1, 0, cam.nearClipPlane)).x;
-        minY = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane)).y;
-        maxY = cam.ViewportToWorldPoint(new Vector3(0, 1, cam.nearClipPlane)).y;
-
         sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,13 +24,16 @@ public class Movement : MonoBehaviour
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
-       
-        float spriteHalfWidth = sprite.bounds.size.x/2f;
-        float spriteHalfHeight = sprite.bounds.size.y/2f;
         
         moveInput.Normalize();
 
         rb2d.velocity = moveInput*moveSpeed;
+
+        //update animation parameters
+        animator.SetFloat("MoveX", moveInput.x);
+        animator.SetFloat("MoveY", moveInput.y);
+
+        sprite.flipX = moveInput.x==-1;
     }
 
     public void StopMvmt()
